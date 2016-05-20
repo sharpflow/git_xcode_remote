fbd8ba8bc8cad9c305570e771dfa4999e02128f4
sharpflow <sharpflow@gmail.com>
2016-05-20 10:38:09 +0900

    Initial Commit

---
+++ git_xcode_tutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  git_xcode_tutorial
+//
+//  Created by 김호철 on 2016. 5. 20..
+//  Copyright © 2016년 김호철. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
